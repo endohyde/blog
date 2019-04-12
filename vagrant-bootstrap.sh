0cb90e9952bafdfc6a18127216b0aa65eae6ba30
mox <mox@teteze-rise.teteze-rise>
2019-04-13 04:02:02 +0900

    phpを使えるようにした

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -19,9 +19,35 @@ cd php-7.2.17/
 apt install -y libxml2-dev libbz2-dev libcurl4-openssl-dev libwebp-dev libjpeg-dev libpng-dev libxpm-dev libfreetype6-dev libgmp3-dev libreadline-dev libsnmp-dev libtidy-dev libxslt-dev snmp
 
 ./configure --prefix=/opt/php --with-libdir=lib64 --with-pic --with-bz2 --with-freetype-dir --with-png-dir --with-xpm-dir --with-gettext --with-gmp --with-iconv --with-jpeg-dir --with-curl --with-webp-dir --with-png-dir --with-openssl --with-pcre-regex --with-zlib --with-layout=GNU --enable-exif --enable-ftp --enable-sockets --with-kerberos --enable-shmop --enable-calendar --with-libxml-dir --with-mhash --with-readline --with-snmp --with-tidy --with-xsl --with-gnu-ld --enable-mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-mysql-sock=/var/lib/mysql/mysql.sock --enable-mbstring --with-gd --enable-fpm
-make
-sudo make install
+make -j7
+make install
 
 export PATH=$PATH:/opt/php/bin
 export PATH=$PATH:/opt/php/sbin
 echo "PATH=$PATH" > /etc/environment
+
+cp /vagrant/vagrant/php-fpm.conf /opt/php/etc/
+cp /vagrant/vagrant/www.conf /opt/php/etc/php-fpm.d/
+
+chmod -R 775 /opt/
+chown -R www-data /opt/
+mkdir /var/run/php-fpm/
+chown -R www-data /var/run/php-fpm/
+chmod a+x /opt/php/sbin/php-fpm
+
+cp /vagrant/vagrant/php-fpm.service /lib/systemd/system/php-fpm.service
+
+cp /vagrant/vagrant/web-server.conf /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/web-server.conf /etc/nginx/sites-enabled/web-server.conf
+rm /etc/nginx/sites-enabled/default
+
+systemctl stop nginx
+
+systemctl enable nginx
+systemctl enable php-fpm
+systemctl start php-fpm
+systemctl start nginx
+
+echo "--------------------------"
+echo "  vagrant up success!!"
+echo "--------------------------"
